97334193b5496bc8bffba5874a8fb83eefd1eeee
Vikram Tiwari <Vikramtheone1@gmail.com>
2014-09-28 03:27:18 +0530

    Added sponsors :tent:
    Images and other content

--- static/js/config.js
+++ static/js/config.js
@@ -7,8 +7,9 @@ boomerang.factory('Config', function () {
         'pwa_id'        : '', //picasa web album id, must belong to google+ id above
         'domain'        : 'http://gdga-site.appspot.com',
         'cover' : {
-            title: 'Google Developers Group - Allahabad',
+            title: 'GDG - Allahabad',
             subtitle: 'In-person, local developer community groups near you.',
+            url: 'https://lh4.googleusercontent.com/-Clf8h-YubQY/VBugyeTDmAI/AAAAAAAAAUk/1XtD8jZxsq8/s761-fcrop64=1,00001f97ffffe0f3/10655336_750057261718566_463158107335915810_o.jpg',
             button: {
                 text: 'Find out more',
                 url: 'https://developers.google.com/groups/chapter/102753048484564707718/'
